0eed8f1bd6891514be3d4e125a711e24bd2633e9
skuhlmann <samkuhlmann@gmail.com>
2024-05-03 11:34:55 -0600

    gets fert deployed and verified

--- contracts/shamans/PeachTycoonFertilizer.sol
+++ contracts/shamans/PeachTycoonFertilizer.sol
@@ -11,31 +11,25 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 contract PeachTycoonFertilzer is ReentrancyGuard, Initializable, Ownable {
     string public constant name = "PeachTycoonFertilzer";
 
-    uint256 public boostEnd = 1717110000;
-    uint256 public boostPrice = 1000000000000000;
-    uint256 public boostPriceERC20 = 1000000000000000;
-    uint256 private boostDiscount = 100000000000000;
-    uint256 private boostDiscountERC20 = 100000000000000;
-    // address public farmAccount = 0xB1344e792dd923486B7b9665f05454f6A6872A4b; /* Address of farm safe */
-    // address public farmerCoopAccount = 0xe172278c17F0E58124F2b3201562348FF677c365; /* Address of farmer's coop safe */
-    address public farmAccount = 0xf100041473280B594D78AB5Fa4C44Ba81edd367B; /* Address of farm safe */
-    address public farmerCoopAccount = 0xf100041473280B594D78AB5Fa4C44Ba81edd367B; /* Address of farmer's coop safe */
-    uint256 private farmerCoopCut = 100000000000000; /* Farmer co-op cut */
-    uint256 private farmerCoopCutERC20 = 100000000000000; /* Farmer co-op cut */
-    uint256 private lootPerBoost = 75000000000000000000; /* Loot per fertilizer */
-    uint256 private boostPerToken = 1; /* Ferts per token */
+    uint256 public boostEnd = 1717221600;
+    uint256 public boostPrice = 35000000000000000;
+    uint256 public boostPriceERC20 = 5000000000000000000000;
+    uint256 public boostDiscount = 8750000000000000;
+    uint256 public boostDiscountERC20 = 1250000000000000000000;
+    address public farmAccount = 0xB1344e792dd923486B7b9665f05454f6A6872A4b; /* Address of farm safe */
+    address public farmerCoopAccount = 0xe172278c17F0E58124F2b3201562348FF677c365; /* Address of farmer's coop safe */
+    uint256 public farmerCoopCut = 1050000000000000; /* Farmer co-op cut */
+    uint256 public farmerCoopCutERC20 = 150000000000000000000; /* Farmer co-op cut */
+    uint256 public lootPerBoost = 100000000000000000000; /* Loot per fertilizer */
+    uint256 public boostPerToken = 1; /* Ferts per token */
     uint256 public totalFertilizations = 0; /* Count of all ferts */
 
-    // IBaal public baal = IBaal(0x1503Bd5f6F082F7fBD36438CC416CD67849c0Bec);
-    // IERC721 public treeNft = IERC721(0xA9d3c833df8415233e1626F29E33ccBA37d2A187);
-    // IERC20 public paymentERC20 = IERC20(0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed);
-    // IERC20 public discountERC20 = IERC20(0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed);
-    IBaal public baal = IBaal(0x112e54a494FCA06D71a5b253c9DDdbA6Dc9267FF);
-    IERC721 public treeNft = IERC721(0xB49a877D82c1f0133B0293dfd20eB54BEd07a290);
-    IERC20 public paymentERC20 = IERC20(0x53c8156592A64E949A4736c6D3309002fa0b2Aba);
-    IERC20 public discountERC20 = IERC20(0x8FAE2f5B1eD1a73ee7c96703AeaCc735Fbe82Ed0);
+    IBaal public baal = IBaal(0x1503Bd5f6F082F7fBD36438CC416CD67849c0Bec);
+    IERC721 public treeNft = IERC721(0xA9d3c833df8415233e1626F29E33ccBA37d2A187);
+    IERC20 public paymentERC20 = IERC20(0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed);
+    IERC20 public discountERC20 = IERC20(0xe68E3B3Ee0ce43d55fedCB625D48af4601693a04);
 
-    mapping(uint256 => uint8) public fertlizations; /*maps `tokenId` to fertlizations count*/
+    mapping(uint256 => uint8) public fertilizations; /*maps `tokenId` to fertilizations count*/
 
     function _mintTokens(address to, uint256 amount) private {
         address[] memory _receivers = new address[](1);
@@ -47,16 +41,18 @@ contract PeachTycoonFertilzer is ReentrancyGuard, Initializable, Ownable {
     }
 
     function fertilize(uint256 _tokenId) public payable nonReentrant {
-        require(boostEnd > block.timestamp, "fertlize period has ended");
-        require(msg.value == boostPrice, "incorrect payment amount");
-        require(fertlizations[_tokenId] < boostPerToken, "tree has already been fertlized");
+        require(boostEnd > block.timestamp, "fertlization period has ended");
+        require(fertilizations[_tokenId] < boostPerToken, "tree has already been fertlized");
         require(baal.isManager(address(this)), "shaman not manager");
         require(treeNft.ownerOf(_tokenId) == msg.sender, "sender not tree owner");
 
         uint amountMinusFee = msg.value - farmerCoopCut;
 
         if (discountERC20.balanceOf(msg.sender) > 0) {
+            require(msg.value == (boostPrice - boostDiscount), "incorrect payment amount");
             amountMinusFee -= boostDiscount;
+        } else {
+            require(msg.value == boostPrice, "incorrect eth payment amount");
         }
 
         (bool feeSent, ) = farmerCoopAccount.call{ value: farmerCoopCut }("");
@@ -65,23 +61,25 @@ contract PeachTycoonFertilzer is ReentrancyGuard, Initializable, Ownable {
         (bool sent, ) = farmAccount.call{ value: amountMinusFee }("");
         require(sent, "ETH not sent");
 
-        fertlizations[_tokenId] += 1;
+        fertilizations[_tokenId] += 1;
         totalFertilizations += 1;
 
         _mintTokens(msg.sender, lootPerBoost);
     }
 
     function fertilizeERC20(uint256 _tokenId, uint256 _amount) public nonReentrant {
-        require(boostEnd > block.timestamp, "fertlize period has ended");
-        require(_amount == boostPriceERC20, "incorrect payment amount");
-        require(fertlizations[_tokenId] < boostPerToken, "tree has already been fertlized");
+        require(boostEnd > block.timestamp, "fertlization period has ended");
+        require(fertilizations[_tokenId] < boostPerToken, "tree has already been fertlized");
         require(baal.isManager(address(this)), "shaman not manager");
         require(treeNft.ownerOf(_tokenId) == msg.sender, "sender not tree owner");
 
         uint amountMinusFee = _amount - farmerCoopCutERC20;
 
         if (discountERC20.balanceOf(msg.sender) > 0) {
+            require(_amount == (boostPriceERC20 - boostDiscountERC20), "incorrect erc20 payment amount");
             amountMinusFee -= boostDiscountERC20;
+        } else {
+            require(_amount == boostPriceERC20, "incorrect erc20 payment amount");
         }
 
         require(
@@ -91,7 +89,7 @@ contract PeachTycoonFertilzer is ReentrancyGuard, Initializable, Ownable {
 
         require(paymentERC20.transferFrom(msg.sender, address(farmAccount), amountMinusFee), "Payment Transfer failed");
 
-        fertlizations[_tokenId] += 1;
+        fertilizations[_tokenId] += 1;
         totalFertilizations += 1;
 
         _mintTokens(msg.sender, lootPerBoost);
@@ -103,5 +101,21 @@ contract PeachTycoonFertilzer is ReentrancyGuard, Initializable, Ownable {
 
     function setBoostPerToken(uint256 _newBoostPerToken) public onlyOwner {
         boostPerToken = _newBoostPerToken;
+    }
+
+    function setPrices(
+        uint256 _newBoostPrice,
+        uint256 _newBoostPriceERC20,
+        uint256 _newBoostDiscount,
+        uint256 _newBoostDiscountERC20,
+        uint256 _newFarmerCoopCut,
+        uint256 _newFarmerCoopCutERC20
+    ) public onlyOwner {
+        boostPrice = _newBoostPrice;
+        boostPriceERC20 = _newBoostPriceERC20;
+        boostDiscount = _newBoostDiscount;
+        boostDiscountERC20 = _newBoostDiscountERC20;
+        farmerCoopCut = _newFarmerCoopCut;
+        farmerCoopCutERC20 = _newFarmerCoopCutERC20;
     }
 }
--- contracts/shamans/PeachTycoonSprayer.sol
+++ contracts/shamans/PeachTycoonSprayer.sol
@@ -11,27 +11,36 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 contract PeachTycoonSprayer is ReentrancyGuard, Initializable, Ownable {
     string public constant name = "PeachTycoonSprayer";
 
-    uint256 public boostEnd = 1713830400;
-    uint256 public boostPrice = 22000000000000000;
-    uint256 public boostPriceERC20 = 5200000000000000000000;
-    address public farmAccount = 0xB1344e792dd923486B7b9665f05454f6A6872A4b; /* Address of farm safe */
-    address public farmerCoopAccount = 0xe172278c17F0E58124F2b3201562348FF677c365; /* Address of farmer's coop safe */
-    uint256 public farmerCoopCut = 660000000000000; /* Farmer co-op cut */
-    uint256 public farmerCoopCutERC20 = 156000000000000000000; /* Farmer co-op cut */
-    uint256 public lootPerBoost = 75000000000000000000; /* Loot per pruning */
-    uint256 public attemptsPerToken = 3; /* Attempts per token*/
-    uint256 public totalSprayWins = 0; /* Count of all spary wins */
+    uint256 public boostEnd = 1717110000;
+    uint256 public boostPrice = 1000000000000000;
+    uint256 public boostPriceERC20 = 1000000000000000;
+    // address public farmAccount = 0xB1344e792dd923486B7b9665f05454f6A6872A4b; /* Address of farm safe */
+    // address public farmerCoopAccount = 0xe172278c17F0E58124F2b3201562348FF677c365; /* Address of farmer's coop safe */
+    address public farmAccount = 0xf100041473280B594D78AB5Fa4C44Ba81edd367B; /* Address of farm safe */
+    address public farmerCoopAccount = 0xf100041473280B594D78AB5Fa4C44Ba81edd367B; /* Address of farmer's coop safe */
+    uint256 private farmerCoopCut = 100000000000000; /* Farmer co-op cut */
+    uint256 private farmerCoopCutERC20 = 100000000000000; /* Farmer co-op cut */
+    uint256 private lootPerBoost = 75000000000000000000; /* Loot per fertilizer */
+    uint256 public attemptsPerToken = 2; /* Attempts per token*/
+    uint256 public totalSprayWins = 0; /* Count of all spray wins */
+    uint256 public totalSprayAttempts = 0; /* Count of all spray attempts */
+
     uint256 private bugs;
 
-    IBaal public baal = IBaal(0x1503Bd5f6F082F7fBD36438CC416CD67849c0Bec);
-    IERC721 public treeNft = IERC721(0xA9d3c833df8415233e1626F29E33ccBA37d2A187);
-    IERC20 public paymentERC20 = IERC20(0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed);
+    // IBaal public baal = IBaal(0x1503Bd5f6F082F7fBD36438CC416CD67849c0Bec);
+    // IERC721 public treeNft = IERC721(0xA9d3c833df8415233e1626F29E33ccBA37d2A187);
+    // IERC20 public paymentERC20 = IERC20(0x4ed4E862860beD51a9570b96d89aF5E1B0Efefed);
+    IBaal public baal = IBaal(0x112e54a494FCA06D71a5b253c9DDdbA6Dc9267FF);
+    IERC721 public treeNft = IERC721(0xB49a877D82c1f0133B0293dfd20eB54BEd07a290);
+    IERC20 public paymentERC20 = IERC20(0x53c8156592A64E949A4736c6D3309002fa0b2Aba);
 
     mapping(uint256 => uint8) public sprayAttempts; /*maps `tokenId` to spray attempt count*/
     mapping(uint256 => uint8) public sprayWins; /*maps `tokenId` to spray win count*/
 
+    event Sprayed(uint256 tokenId, bool win, uint256 attempt);
+
     constructor() {
-        bugs = (block.timestamp + block.prevrandao) % 100;
+        bugs = block.timestamp % 100;
     }
 
     function _mintTokens(address to, uint256 amount) private {
@@ -43,7 +52,7 @@ contract PeachTycoonSprayer is ReentrancyGuard, Initializable, Ownable {
         baal.mintLoot(_receivers, _amounts);
     }
 
-    function fertilize(uint256 _tokenId) public payable nonReentrant {
+    function spray(uint256 _tokenId) public payable nonReentrant {
         require(boostEnd > block.timestamp, "fertlize period has ended");
         require(msg.value == boostPrice, "incorrect payment amount");
         require(sprayAttempts[_tokenId] < attemptsPerToken, "tree has already been fertlized");
@@ -59,19 +68,19 @@ contract PeachTycoonSprayer is ReentrancyGuard, Initializable, Ownable {
         require(sent, "ETH not sent");
 
         sprayAttempts[_tokenId] += 1;
+        totalSprayAttempts += 1;
 
         bool wonRoll = roll();
         if (wonRoll) {
             sprayWins[_tokenId] += 1;
             totalSprayWins += 1;
-
-            _mintTokens(msg.sender, lootPerBoost);
-        } else {
-            sprayAttempts[_tokenId] += 1;
         }
+        _mintTokens(msg.sender, lootPerBoost);
+
+        emit Sprayed(_tokenId, wonRoll, sprayAttempts[_tokenId] - 1);
     }
 
-    function fertilizeERC20(uint256 _tokenId, uint256 _amount) public nonReentrant {
+    function sprayERC20(uint256 _tokenId, uint256 _amount) public nonReentrant {
         require(boostEnd > block.timestamp, "fertlize period has ended");
         require(_amount == boostPriceERC20, "incorrect payment amount");
         require(sprayAttempts[_tokenId] < attemptsPerToken, "tree has already been fertlized");
@@ -88,22 +97,28 @@ contract PeachTycoonSprayer is ReentrancyGuard, Initializable, Ownable {
         require(paymentERC20.transferFrom(msg.sender, address(farmAccount), amountMinusFee), "Payment Transfer failed");
 
         sprayAttempts[_tokenId] += 1;
+        totalSprayAttempts += 1;
 
         bool wonRoll = roll();
         if (wonRoll) {
             sprayWins[_tokenId] += 1;
             totalSprayWins += 1;
-
-            _mintTokens(msg.sender, lootPerBoost);
         }
+        _mintTokens(msg.sender, lootPerBoost);
+
+        emit Sprayed(_tokenId, wonRoll, sprayAttempts[_tokenId] - 1);
     }
 
-    function setBoostEnd(uint256 _newboostEnd) public onlyOwner {
-        boostEnd = _newboostEnd;
+    function setBoostEnd(uint256 _newBoostEnd) public onlyOwner {
+        boostEnd = _newBoostEnd;
+    }
+
+    function setAttemptsPerToken(uint256 _newAttemptsPerToken) public onlyOwner {
+        attemptsPerToken = _newAttemptsPerToken;
     }
 
     function roll() private returns (bool) {
-        bugs = (bugs + block.timestamp + block.prevrandao) % 100;
+        bugs = (bugs + block.timestamp) % 100;
         return bugs <= 33;
     }
 }
